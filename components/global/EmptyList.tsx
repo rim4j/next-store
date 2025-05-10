f6fae74c862dad81338e59072b4edb9ff7754575
rim4j <amirhosseinjalali818@gmail.com>
2025-05-10 16:30:00 +0330

    sectionTitle and emptyList component

--- components/global/EmptyList.tsx
+++ components/global/EmptyList.tsx
@@ -1,5 +1,13 @@
-const EmptyList = () => {
-  return <div>EmptyList</div>;
+import { cn } from "@/lib/utils";
+
+const EmptyList = ({
+  heading = "No items found.",
+  className,
+}: {
+  heading?: string;
+  className?: string;
+}) => {
+  return <h2 className={cn("text-xl", className)}>{heading}</h2>;
 };
 
 export default EmptyList;
--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,5 +1,14 @@
-const SectionTitle = () => {
-  return <div>SectionTitle</div>;
+import { Divider } from "@heroui/divider";
+
+const SectionTitle = ({ title }: { title: string }) => {
+  return (
+    <div>
+      <h2 className='text-3xl font-medium tracking-wider capitalize mb-8'>
+        {title}
+      </h2>
+      <Divider />
+    </div>
+  );
 };
 
 export default SectionTitle;

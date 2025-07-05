971b61a8a93d7e7a9f5b6a83c43940255fbd52ad
rim4j <amirhosseinjalali818@gmail.com>
2025-07-05 08:09:03 +0330

    fetch admin product image

--- utils/actions.ts
+++ utils/actions.ts
@@ -124,3 +124,30 @@ export const deleteProductAction = async (prevState: { productId: string }) => {
     return renderError(error);
   }
 };
+
+export const fetchAdminProductDetails = async (productId: string) => {
+  await getAdminUser();
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+
+  if (!product) redirect("/admin/products");
+
+  return product;
+};
+
+export const updateProductAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  return { message: "update product successfully" };
+};
+
+export const updateProductImageAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  return { message: "update product image successfully" };
+};

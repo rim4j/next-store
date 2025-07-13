5daa189219007c4ff254957926755d3dd1ed3dfc
rim4j <amirhosseinjalali818@gmail.com>
2025-07-13 08:21:47 +0330

    create loading table

---
+++ components/global/LoadingTable.tsx
@@ -0,0 +1,15 @@
+import { Skeleton } from "@heroui/skeleton";
+
+const LoadingTable = ({ rows = 5 }: { rows?: number }) => {
+  const tableRows = Array.from({ length: rows }, (_, index) => {
+    return (
+      <div key={index} className='mb-4'>
+        <Skeleton className='w-full h-8 rounded' />
+      </div>
+    );
+  });
+
+  return <>{tableRows}</>;
+};
+
+export default LoadingTable;
